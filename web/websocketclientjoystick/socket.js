9b9aecd4b6ee185b3d201d28d7e873eb3870ca97
Emiliano <emiriosgavag@gmail.com>
2019-05-27 18:21:22 -0300

    Implementacion inicial de la barra de vida

--- web/websocketclientjoystick/socket.js
+++ web/websocketclientjoystick/socket.js
@@ -7,7 +7,23 @@ window.onload = function() {
   // Define la url del servidor como la hostname de la pagina y el puerto definido 8080 del ws
   var url = "ws://" + page.hostname + ":8080";
   socket = new WebSocket(url + "/Edimbrujo/GameWebSocket");
+  socket.onmessage = stateUpdate;
 
-  // Este cliente no recibe mensajes, solo envia
-  // socket.onmessage = stateUpdate;
+  function stateUpdate(event) {
+    var gameState = JSON.parse(event.data);
+    if (typeof gameState !== "undefined") {
+      if (gameState["id"] !== "undefined" && socketID === "") {
+        socketID = gameState["id"];
+        //console.log(socketID);
+      }
+      var i = 0;
+      while (typeof gameState[i] !== "undefined") {
+        //console.log(game2State[i]);
+        if (typeof gameState[i]["Health"] !== "undefined") {
+          var health = gameState[i]["Health"];
+          updateHealth(health);
+        }
+      }
+    }
+  }
 };
